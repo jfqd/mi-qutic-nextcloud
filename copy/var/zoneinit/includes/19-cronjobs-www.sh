c75cba37c6e64b10c561429eade68773480f14b2
jfqd <jfqd@blun.org>
2018-01-06 20:10:46 +0100

    fix cron path

--- copy/var/zoneinit/includes/19-cronjobs-www.sh
+++ copy/var/zoneinit/includes/19-cronjobs-www.sh
@@ -1,4 +1,4 @@
 # create cronjob for nexcloud script
-echo '0,10,20,30,40,50 * * * * /opt/local/bin/php /opt/local/share/httpd/nextcloud/current/cron.php' >> /var/spool/cron/crontabs/www
+echo '0,10,20,30,40,50 * * * * /opt/local/bin/php /var/www/htdocs/nextcloud/current/cron.php' >> /var/spool/cron/crontabs/www
 echo "0,15,30,45 * * * * /usr/bin/find /var/tmp/ -name sess_* -cmin +120 -exec rm '{}' +" >> /var/spool/cron/crontabs/www
 echo "# End" >> /var/spool/cron/crontabs/www
