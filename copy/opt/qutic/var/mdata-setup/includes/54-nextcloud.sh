16014bddc8049edd1d1d83ef03d458bcfa6826c8
jfqd <jfqd@blun.org>
2018-03-20 07:41:39 +0100

    fix server_alias

--- copy/opt/qutic/var/mdata-setup/includes/54-nextcloud.sh
+++ copy/opt/qutic/var/mdata-setup/includes/54-nextcloud.sh
@@ -14,9 +14,11 @@ fi
 if mdata-get server_name 1>/dev/null 2>&1; then
   SERVER_NAME=`mdata-get server_name`
   sed -i "s:SERVER_NAME:$SERVER_NAME:g" /opt/local/etc/httpd/vhosts/01-nextcloud.conf
+else
+  sed -i "s:ServerName SERVER_NAME::g" /opt/local/etc/httpd/vhosts/01-nextcloud.conf
 fi
 
-if mdata-get server_name 1>/dev/null 2>&1; then
+if mdata-get server_alias 1>/dev/null 2>&1; then
   SERVER_ALIAS=`mdata-get server_alias`
   sed -i "s:SERVER_ALIAS:$SERVER_ALIAS:g" /opt/local/etc/httpd/vhosts/01-nextcloud.conf
 else
