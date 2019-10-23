82eba97ef272cedd542c8f9bf8d299042a0350c2
jfqd <jfqd@blun.org>
2019-10-23 19:41:01 +0200

    reactivate spiped connection

--- copy/var/zoneinit/includes/35-spiped.sh
+++ copy/var/zoneinit/includes/35-spiped.sh
@@ -14,15 +14,15 @@ if mdata-get spiped_key 1>/dev/null 2>&1; then
   #   svccfg import /opt/local/lib/svc/manifest/spiped-redis-fallback.xml
   # fi
   
-  # if mdata-get spiped_percona_host 1>/dev/null 2>&1; then
-  #   SPIPED_PERCONA_HOST=`mdata-get spiped_percona_host`
-  #   sed -i "s/-t 127.0.0.1:23306/-t $SPIPED_PERCONA_HOST:23306/g" /opt/local/lib/svc/manifest/spiped-percona.xml
-  #   svccfg import /opt/local/lib/svc/manifest/spiped-percona.xml
-  # fi
+  if mdata-get spiped_percona_host 1>/dev/null 2>&1; then
+    SPIPED_PERCONA_HOST=`mdata-get spiped_percona_host`
+    sed -i "s/-t 127.0.0.1:23306/-t $SPIPED_PERCONA_HOST:23306/g" /opt/local/lib/svc/manifest/spiped-percona.xml
+    svccfg import /opt/local/lib/svc/manifest/spiped-percona.xml
+  fi
   
-  # if mdata-get spiped_percona_fallback_host 1>/dev/null 2>&1; then
-  #   SPIPED_PERCONA_FALLBACK_HOST=`mdata-get spiped_percona_fallback_host`
-  #   sed -i "s/-t 127.0.0.1:23306/-t $SPIPED_PERCONA_FALLBACK_HOST:23306/g" /opt/local/lib/svc/manifest/spiped-percona-fallback.xml
-  #   svccfg import /opt/local/lib/svc/manifest/spiped-percona-fallback.xml
-  # fi
+  if mdata-get spiped_percona_fallback_host 1>/dev/null 2>&1; then
+    SPIPED_PERCONA_FALLBACK_HOST=`mdata-get spiped_percona_fallback_host`
+    sed -i "s/-t 127.0.0.1:23306/-t $SPIPED_PERCONA_FALLBACK_HOST:23306/g" /opt/local/lib/svc/manifest/spiped-percona-fallback.xml
+    svccfg import /opt/local/lib/svc/manifest/spiped-percona-fallback.xml
+  fi
 fi
