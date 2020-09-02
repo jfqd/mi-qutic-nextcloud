2b1ace5696f8ef1d4f92e783865316eeff29fb3a
jfqd <jfqd@blun.org>
2020-09-02 12:15:50 +0200

    add todo

--- copy/var/zoneinit/includes/46-apache.sh
+++ copy/var/zoneinit/includes/46-apache.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+# TODO
+# set MaxRequestWorkers and ServerLimit
+# by script related to available memory
+
 # Setup hostname
 sed -i "s:HOSTNAME:$(hostname):g" /opt/local/etc/httpd/vhosts/00-default.conf
