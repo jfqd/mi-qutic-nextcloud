2c545dd8ea0b116c35631e6af12bfa963843dc8c
Stefan Husch <jfqd@blun.org>
2021-01-16 13:28:41 +0100

    skip php cron output

--- copy/var/zoneinit/includes/19-cronjobs-www.sh
+++ copy/var/zoneinit/includes/19-cronjobs-www.sh
@@ -1,5 +1,5 @@
 # create cronjob for nexcloud script
-echo '0,10,20,30,40,50 * * * * /opt/local/bin/php /var/www/htdocs/nextcloud/current/cron.php' >> /var/spool/cron/crontabs/www
+echo '0,10,20,30,40,50 * * * * /opt/local/bin/php /var/www/htdocs/nextcloud/current/cron.php > /dev/null' >> /var/spool/cron/crontabs/www
 echo "0,15,30,45 * * * * /usr/bin/find /var/tmp/ -name sess_* -cmin +120 -exec rm '{}' +" >> /var/spool/cron/crontabs/www
 
 CRONJOB_ALL_FILES=`mdata-get cronjob_all_files`
