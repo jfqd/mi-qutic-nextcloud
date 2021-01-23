6e53bfaeae2db6d808b900a80f89f0f2deb9849c
Stefan Husch <jfqd@blun.org>
2021-01-23 11:32:21 +0100

    include vhost fpm settings

--- copy/var/zoneinit/includes/42-php-fpm.sh
+++ copy/var/zoneinit/includes/42-php-fpm.sh
@@ -9,6 +9,10 @@ if [[ mdata-get mpm_event = "true" ]]; then
   mv /opt/local/etc/httpd/conf.d/00-modules_event.bak /opt/local/etc/httpd/conf.d/00-modules_event.conf
   mv /opt/local/etc/httpd/conf.d/09-tuning_event.bak /opt/local/etc/httpd/conf.d/09-tuning_event.conf
 
+  # http-vhost
+  mv /opt/local/etc/httpd/vhosts/01-nextcloud.conf /opt/local/etc/httpd/vhosts/01-nextcloud.bak
+  mv /opt/local/etc/httpd/vhosts/01-nextcloud-fpm.bak /opt/local/etc/httpd/vhosts/01-nextcloud-fpm.conf
+
   # Configure PHP sendmail return-path if possible
   if mdata-get mail_adminaddr 1>/dev/null 2>&1; then
     echo "php_admin_value[sendmail_path] = /usr/sbin/sendmail -t -i -f $(mdata-get mail_adminaddr)" \
