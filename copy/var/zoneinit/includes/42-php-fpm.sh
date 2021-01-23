12f8092952f7765270d33635e35a74585c03ee9e
Stefan Husch <jfqd@blun.org>
2021-01-23 16:58:48 +0100

    deactivate php-module

--- copy/var/zoneinit/includes/42-php-fpm.sh
+++ copy/var/zoneinit/includes/42-php-fpm.sh
@@ -5,13 +5,12 @@ if [[ mdata-get mpm_event = "true" ]]; then
   mv /opt/local/etc/httpd/conf.d/00-modules_prefork.conf /opt/local/etc/httpd/conf.d/00-modules_prefork.bak
   mv /opt/local/etc/httpd/conf.d/09-tuning_prefork.conf /opt/local/etc/httpd/conf.d/09-tuning_prefork.bak
 
+  # Deactivate php-module
+  mv /opt/local/etc/httpd/conf.d/10-php.conf /opt/local/etc/httpd/conf.d/10-php.bak
+  
   # Activate apache mpm-event files
   mv /opt/local/etc/httpd/conf.d/00-modules_event.bak /opt/local/etc/httpd/conf.d/00-modules_event.conf
   mv /opt/local/etc/httpd/conf.d/09-tuning_event.bak /opt/local/etc/httpd/conf.d/09-tuning_event.conf
-
-  # http-vhost
-  # mv /opt/local/etc/httpd/vhosts/01-nextcloud.conf /opt/local/etc/httpd/vhosts/01-nextcloud.bak
-  # mv /opt/local/etc/httpd/vhosts/01-nextcloud-fpm.bak /opt/local/etc/httpd/vhosts/01-nextcloud-fpm.conf
 
   # Configure PHP sendmail return-path if possible
   if mdata-get mail_adminaddr 1>/dev/null 2>&1; then
