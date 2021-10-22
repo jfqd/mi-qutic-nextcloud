ae386086e7e2961a04a1f5bcee16eeb1e69446df
jfqd <jfqd@blun.org>
2021-10-22 15:12:37 +0200

    refactor php-fpm, nginx and apache config

--- copy/var/zoneinit/includes/42-php-fpm.sh
+++ copy/var/zoneinit/includes/42-php-fpm.sh
@@ -1,17 +1,43 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
+#################################
+# nginx config
+#
+if mdata-get mail_adminaddr 1>/dev/null 2>&1; then
+  # Configure PHP sendmail return-path if possible
+  echo "php_admin_value[sendmail_path] = /usr/sbin/sendmail -t -i -f $(mdata-get mail_adminaddr)" \
+    >> /opt/local/etc/php-fpm.d/www.conf
+fi
+
+sed -i "s:nc.example.com:$(mdata-get server_name):g" /opt/local/etc/nginx/nginx.conf
+sed -i "s:HOSTNAME:$(hostname):g" /opt/local/etc/httpd/vhosts/00-default.conf
+
+#################################
+# apache config
+#
+if mdata-get server_name 1>/dev/null 2>&1; then
+  SERVER_NAME=`mdata-get server_name`
+  sed -i "s:SERVER_NAME:$SERVER_NAME:g" /opt/local/etc/httpd/vhosts/01-nextcloud.conf
+else
+  sed -i "s:ServerName SERVER_NAME::g" /opt/local/etc/httpd/vhosts/01-nextcloud.conf
+fi
+
+if mdata-get server_alias 1>/dev/null 2>&1; then
+  SERVER_ALIAS=`mdata-get server_alias`
+  sed -i "s:SERVER_ALIAS:$SERVER_ALIAS:g" /opt/local/etc/httpd/vhosts/01-nextcloud.conf
+else
+  sed -i "s:ServerAlias SERVER_ALIAS::g" /opt/local/etc/httpd/vhosts/01-nextcloud.conf
+fi
+
+echo "/opt/local/bin/sed -i \"s#/var/log/php-fpm.log.*##\" /etc/logadm.conf" >> /opt/local/bin/uptodate
+
+#################################
+# now decide which one to use
+#
 if mdata-get nginx_fpm 1>/dev/null 2>&1; then
   if [[ `mdata-get nginx_fpm` = "true" ]]; then
     # Disable Apache
     /usr/sbin/svcadm disable apache
-
-    # Configure PHP sendmail return-path if possible
-    if mdata-get mail_adminaddr 1>/dev/null 2>&1; then
-      echo "php_admin_value[sendmail_path] = /usr/sbin/sendmail -t -i -f $(mdata-get mail_adminaddr)" \
-        >> /opt/local/etc/php-fpm.d/www.conf
-    fi
-    
-    sed -i "s:nc.example.com:$(mdata-get server_name):g" /opt/local/etc/nginx/nginx.conf
 
     # Enable PHP-FPM
     /usr/sbin/svcadm enable svc:/pkgsrc/php-fpm:default
@@ -19,23 +45,5 @@ if mdata-get nginx_fpm 1>/dev/null 2>&1; then
   fi
 else
   # Enable apache
-  sed -i "s:HOSTNAME:$(hostname):g" /opt/local/etc/httpd/vhosts/00-default.conf
-  
-  if mdata-get server_name 1>/dev/null 2>&1; then
-    SERVER_NAME=`mdata-get server_name`
-    sed -i "s:SERVER_NAME:$SERVER_NAME:g" /opt/local/etc/httpd/vhosts/01-nextcloud.conf
-  else
-    sed -i "s:ServerName SERVER_NAME::g" /opt/local/etc/httpd/vhosts/01-nextcloud.conf
-  fi
-
-  if mdata-get server_alias 1>/dev/null 2>&1; then
-    SERVER_ALIAS=`mdata-get server_alias`
-    sed -i "s:SERVER_ALIAS:$SERVER_ALIAS:g" /opt/local/etc/httpd/vhosts/01-nextcloud.conf
-  else
-    sed -i "s:ServerAlias SERVER_ALIAS::g" /opt/local/etc/httpd/vhosts/01-nextcloud.conf
-  fi
-
-  echo "/opt/local/bin/sed -i \"s#/var/log/php-fpm.log.*##\" /etc/logadm.conf" >> /opt/local/bin/uptodate
-
   /usr/sbin/svcadm enable svc:/pkgsrc/apache:default
 fi
