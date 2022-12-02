4cd65ad53d5e66f311952e4414a7cfb7cafe2fef
jfqd <jfqd@blun.org>
2022-12-02 10:27:47 +0100

    cleanup

--- copy/var/zoneinit/includes/33-redis.sh
+++ copy/var/zoneinit/includes/33-redis.sh
@@ -2,14 +2,10 @@
 chown root:redis /opt/local/etc/redis.conf
 chmod 0640 /opt/local/etc/redis.conf
 # local use only
-if mdata-get spiped_redis_host 1>/dev/null 2>&1; then
-  echo "* Use spiped-redis-host"
-else
-  echo "* Activate local redis"
-  gsed -i \
-       -e "s/# maxmemory <bytes>/maxmemory 256mb/" \
-       -e "s/# maxmemory-policy noeviction/# maxmemory-policy allkeys-lfu/" \
-       -e "s/# unixsocket \/tmp\/redis.sock/unixsocket \/var\/tmp\/redis.sock/" \
-       /opt/local/etc/redis.conf
-  svcadm enable redis
-fi
+echo "* Activate local redis"
+gsed -i \
+     -e "s/# maxmemory <bytes>/maxmemory 256mb/" \
+     -e "s/# maxmemory-policy noeviction/# maxmemory-policy allkeys-lfu/" \
+     -e "s/# unixsocket \/tmp\/redis.sock/unixsocket \/var\/tmp\/redis.sock/" \
+     /opt/local/etc/redis.conf
+svcadm enable redis
