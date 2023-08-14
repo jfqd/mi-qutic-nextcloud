9c6a7b0886e9ef0a1a60a285968b38706d9e4c5d
jfqd <jfqd@blun.org>
2023-08-14 15:15:29 +0200

    make nginx the default webserver

--- copy/var/zoneinit/includes/42-php-fpm.sh
+++ copy/var/zoneinit/includes/42-php-fpm.sh
@@ -13,42 +13,47 @@ sed -i "s:nc.example.com:$(mdata-get server_name):g" /opt/local/etc/nginx/nginx.
 sed -i "s:HOSTNAME:$(hostname):g" /opt/local/etc/httpd/vhosts/00-default.conf
 
 #################################
-# apache config
+# now decide which one to use:
+# nginx (default) or apache
 #
-if mdata-get server_name 1>/dev/null 2>&1; then
-  SERVER_NAME=`mdata-get server_name`
-  sed -i "s:SERVER_NAME:$SERVER_NAME:g" /opt/local/etc/httpd/vhosts/01-nextcloud.conf
-else
-  sed -i "s:ServerName SERVER_NAME::g" /opt/local/etc/httpd/vhosts/01-nextcloud.conf
-fi
-
-if mdata-get server_alias 1>/dev/null 2>&1; then
-  SERVER_ALIAS=`mdata-get server_alias`
-  sed -i "s:SERVER_ALIAS:$SERVER_ALIAS:g" /opt/local/etc/httpd/vhosts/01-nextcloud.conf
-else
-  sed -i "s:ServerAlias SERVER_ALIAS::g" /opt/local/etc/httpd/vhosts/01-nextcloud.conf
-fi
-
-#################################
-# now decide which one to use
-#
-if mdata-get nginx_fpm 1>/dev/null 2>&1; then
-  if [[ `mdata-get nginx_fpm` = "true" ]]; then
-    # Disable Apache
-    /usr/sbin/svcadm disable apache
-
-    # remove httpd cron-log-helper
-    sed -i \
-        "s|.*/usr/bin/ls -1dt /var/log/httpd/access.*|# removed httpd cron-log-helper|" \
-        /var/spool/cron/crontabs/root
-    
-    # Enable PHP-FPM
-    /usr/sbin/svcadm enable svc:/pkgsrc/php-fpm:default
-    /usr/sbin/svcadm enable nginx
-    echo "/opt/local/bin/sed -i \"s#/var/log/nginx/.*##\" /etc/logadm.conf" >> /opt/local/bin/uptodate
+if [[ `mdata-get nginx_fpm 2>/dev/null` = "false" ]]; then
+  #################################
+  # use apache
+  #
+  if mdata-get server_name 1>/dev/null 2>&1; then
+    SERVER_NAME=`mdata-get server_name`
+    sed -i "s:SERVER_NAME:$SERVER_NAME:g" /opt/local/etc/httpd/vhosts/01-nextcloud.conf
+  else
+    sed -i "s:ServerName SERVER_NAME::g" /opt/local/etc/httpd/vhosts/01-nextcloud.conf
   fi
-else
+
+  if mdata-get server_alias 1>/dev/null 2>&1; then
+    SERVER_ALIAS=`mdata-get server_alias`
+    sed -i "s:SERVER_ALIAS:$SERVER_ALIAS:g" /opt/local/etc/httpd/vhosts/01-nextcloud.conf
+  else
+    sed -i "s:ServerAlias SERVER_ALIAS::g" /opt/local/etc/httpd/vhosts/01-nextcloud.conf
+  fi
+  
   # Enable apache
   /usr/sbin/svcadm enable svc:/pkgsrc/apache:default
-  echo "/opt/local/bin/sed -i \"s#/var/log/php-fpm.log.*##\" /etc/logadm.conf" >> /opt/local/bin/uptodate
+  echo "/opt/local/bin/sed -i \"s#/var/log/php-fpm.log.*##\" /etc/logadm.conf" >> /opt/local/bin/uptodate 
+
+else
+  #################################
+  # use nginx
+  #
+
+  # Disable Apache
+  /usr/sbin/svcadm disable apache || true
+  
+  # Enable PHP-FPM
+  /usr/sbin/svcadm enable svc:/pkgsrc/php-fpm:default
+  /usr/sbin/svcadm enable nginx
+  echo "/opt/local/bin/sed -i \"s#/var/log/nginx/.*##\" /etc/logadm.conf" >> /opt/local/bin/uptodate
+
+  # remove httpd cron-log-helper
+  sed -i \
+      "s|.*/usr/bin/ls -1dt /var/log/httpd/access.*|# removed httpd cron-log-helper|" \
+      /var/spool/cron/crontabs/root
+  
 fi
