b3b4aa8e77e0cb5c1e2fb0d7945a6d076599bb4d
jfqd <jfqd@blun.org>
2023-11-27 10:44:22 +0100

    add option for redis_maxmemory

--- copy/opt/qutic/var/mdata-setup/includes/54-nextcloud.sh
+++ copy/opt/qutic/var/mdata-setup/includes/54-nextcloud.sh
@@ -18,4 +18,14 @@ if mdata-get fpm_max_worker 1>/dev/null 2>&1; then
     -i \
     -e "s#pm.max_children = 10#pm.max_children = ${WORKER}#" \
     /opt/local/etc/php-fpm.d/www.conf
+  svcadm restart svc:/pkgsrc/php-fpm:default
+fi
+
+if mdata-get redis_maxmemory 1>/dev/null 2>&1; then
+  MAXMEMORY=`mdata-get redis_maxmemory`
+  /opt/local/bin/sed \
+    -i \
+    -e "s#maxmemory 256mb#maxmemory ${MAXMEMORY}#" \
+    /opt/local/etc/redis.conf
+  svcadm restart svc:/pkgsrc/redis:default
 fi
